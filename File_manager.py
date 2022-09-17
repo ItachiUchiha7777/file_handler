bd90ceac4f6a24a8e83571fb7478aa6dce0093c0
Rohit <rohitgusain792@gmail,com>
2022-09-17 21:16:28 +0530

    Update File_manager.py

--- File_manager.py
+++ File_manager.py
@@ -14,7 +14,7 @@ def file_rename(diR , dir_list , exx) :
     for i in dir_list :
         c = i.split(".")
         if exx in c :
-            print(f"ye {i} files hai {exx} extension wali. ")
+            print(f"This {i} files has {exx} extension. ")
             old = diR+"\\" + i
             c [1] = new_ext
             new_name = diR +"\\"+ c [0] + "." + c [1]
@@ -22,19 +22,19 @@ def file_rename(diR , dir_list , exx) :
 
 
 def faltu_files_hatana_wala_hero() :
-    print(f"abhi is work directory mai ke andr hu mai: {os.getcwd()}")
-    diR = input("directory ka path copy krke dalo jisme saman hai ya hatana hai: ")
+    print(f"Currently working on this directory: {os.getcwd()}")
+    diR = input("Please copy the path of the directory in which the files are located: ")
     os.chdir(diR)
-    print(f"Okey apne change kr dia ab directory mai ke andr hu mai: {os.getcwd()}")
+    print(f"Directory changed to: {os.getcwd()}")
     dir_list = os.listdir(diR)  # us path mai jojo files hai unki list banayega ye
-    exx = input("eneter apko konse extension wale file se probem hai: ")
-    inp = int(input(f"{exx} extension wali file delete karna chahoge ya rename. if u want to rename the file press 1 and if u want to delete the file press 2: "))
+    exx = input("Enter the extention you want to deal with: ")
+    inp = int(input("Do you want to rename or delete the fil? if u want to rename the file press 1 and if u want to delete the file press 2: "))
     if inp == 2 :
         file_deleter(dir_list , exx , diR)
     elif inp == 1 :
         file_rename(diR , dir_list , exx)
     else :
-        print("abe oo zyada tez na bano jitne option die hai utne mai se dabo warna ghar aa kr marine tumhe")
+        print("Please choose between 1 or 2")
 
 
 faltu_files_hatana_wala_hero()
